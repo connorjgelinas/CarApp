6e772f65078b52604e7177dbb953cb0e2202d983
Connor Gelinas <connorjgelinas@Zipcoder25s-MacBook-Pro.local>
2016-02-18 14:26:03 -0500

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('FeedCtrl', function($scope, $http) {
+ $http.get('/json/cars.json').then(function(resp) {
+    console.log('Success', resp);
+    $scope.car = [];
+    $scope.carsVal = resp.data;
+    // For JSON responses, resp.data contains the result
+  }, function(err) {
+    console.error('ERR', err);
+    // err.status will contain the status code
+  })
+});
